d20321b23df909182138065788c0bca64bc046f6
Cemil Ülay <cemilulay@gmail.com>
2023-05-27 19:49:05 +0200

    Users screen and Conversations screen, Conversation Creation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
+import getUsers from '../actions/getUsers';
 import Sidebar from '../components/sidebar/Sidebar';
+import UsersList from './components/UsersList';
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className='h-full'>{children}</div>
+      <div className='h-full'>
+        <UsersList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
